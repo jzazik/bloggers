92a6b1f2a40ff7c68290cfb16d8a0d8d396ff0f6
Зазирный Евгений <evgeniy.zazirnyi@letovo.ru>
2024-12-16 13:10:40 +0300

    fix

--- app/Console/Commands/UpdateFromGoogleSpreadsheet.php
+++ app/Console/Commands/UpdateFromGoogleSpreadsheet.php
@@ -526,7 +526,7 @@ class UpdateFromGoogleSpreadsheet extends Command
                 if ($table === 'subscriptions' && $this->transaction
                     ->where('customer_id', $customer->customer_id)
                     ->where('product_id', $product->product_id)
-                    ->where(DB::raw('DATE(transaction_date)'), Carbon::parse($action_date)->toDateString())
+                    ->whereBetween('transaction_date', [Carbon::parse($action_date)->subHour()->toDateTimeString(), Carbon::parse($action_date)->addHour()->toDateTimeString()])
                     ->where('price', $sum)
                     ->exists()
                 ) {
